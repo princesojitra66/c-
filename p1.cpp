25aa33448a743584a4cd90adec56e127810dec0f
princesojitra66 <princesojitra3914@gmail.com>
2025-02-04 19:19:29 +0530

    first commit

---
+++ p1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  
+    cout<<"name : prince sojitra"<<endl;
+    cout<<"age : 20"<<endl;
+    cout<<"email : princesojitra24@gmail.com"<<endl;
+    cout<<"address : punagam"<<endl;
+
+    return 0;
+}
