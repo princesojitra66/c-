c983370763b961a83c58e5a6656e5ff06bbf93dd
princesojitra66 <princesojitra3914@gmail.com>
2025-02-05 19:39:15 +0530

    g

---
+++ p2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+  
+   int a;
+   int b;
+   int c;
+   int d;
+   int e;
+
+     cout<<"enter the value of a"<<endl;
+     cin>>a;
+     cout<<"enter the value of b"<<endl;
+     cin>>b;
+     cout<<"enter the value of c"<<endl;
+     cin>>c;
+     cout<<"enter the value of d"<<endl;
+     cin>>d;
+     cout<<"enter the value of e"<<endl;
+     cin>>e;
+    
+     int ans;
+     ans=a+b+c+d+e;
+     cout<<"sum of all number is ="<<ans;
+
+    return 0;
+}
