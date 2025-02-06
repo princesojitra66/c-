111c1a68400574a92899daaaee63df7d1c422921
princesojitra66 <princesojitra3914@gmail.com>
2025-02-06 16:09:07 +0530

    all code

--- p2.cpp
+++ p2.cpp
@@ -2,12 +2,8 @@
 using namespace std;
 int main(){
   
-   int a;
-   int b;
-   int c;
-   int d;
-   int e;
-
+   int a,b,c,d,e;
+  
      cout<<"enter the value of a"<<endl;
      cin>>a;
      cout<<"enter the value of b"<<endl;
@@ -35,5 +31,29 @@ int main(){
     ans3=a/b/c/d/e;
     cout<<"division of all number is ="<<ans3<<endl;
 
+    int ans4;
+    ans4+=a;
+    cout<<"increment of all number is ="<<ans4<<endl;
+
+    int ans5;
+    ans5-=a;
+    cout<<"decrement of all number is ="<<ans5<<endl;
+
+    int ans6;
+    ans6*=a;
+    cout<<"multiplication of all number is ="<<ans6<<endl;
+
+    int ans7;
+    ans7/=a;
+    cout<<"division of all number is ="<<ans7<<endl;  
+
+    int ans8;
+    ans8= ++a;
+    cout<<"unary increment of all number is ="<<ans8<<endl;
+
+    int ans9;
+    ans9= --b;
+    cout<<"unary decrement of all number is ="<<ans9<<endl;
+    
     return 0;
 }
