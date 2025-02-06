d83e30d1138541fab4abadd0f90635a1ac4c0ae8
princesojitra66 <princesojitra3914@gmail.com>
2025-02-06 15:52:47 +0530

    g1

--- p2.cpp
+++ p2.cpp
@@ -21,7 +21,19 @@ int main(){
     
      int ans;
      ans=a+b+c+d+e;
-     cout<<"sum of all number is ="<<ans;
+     cout<<"sum of all number is ="<<ans <<endl;
+
+    int ans1;
+    ans1=a*b*c*d*e;
+    cout<<"multiplication of all number is ="<<ans1<<endl;
+
+    int ans2;
+    ans2=a-b-c-d-e;
+    cout<<"subtraction of all number is ="<<ans2<<endl;
+
+    int ans3;
+    ans3=a/b/c/d/e;
+    cout<<"division of all number is ="<<ans3<<endl;
 
     return 0;
 }
