897d7c4ff8da694f641ce90bc43d234aeb291a01
princesojitra66 <princesojitra3914@gmail.com>
2025-02-12 19:00:54 +0530

    all

---
+++ atoz.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+
+    cout << "enter no a:-";
+    cin >> a;
+    cout << "enter no b:-";
+    cin >> b;
+    cout << "enter no c:-";
+    cin >> c;
+    cout << "enter no d:-";
+    cin >> d;
+    cout << "enter no e:-";
+    cin >> e;
+    cout << "enter no f:-";
+    cin >> f;
+    cout << "enter no g:-";
+    cin >> g;
+    cout << "enter no h:-";
+    cin >> h;
+    cout << "enter no i:-";
+    cin >> i;
+    cout << "enter no j:-";
+    cin >> j;
+    cout << "enter no k:-";
+    cin >> k;
+    cout << "enter no l:-";
+    cin >> l;
+    cout << "enter no m:-";
+    cin >> m;
+    cout << "enter no n:-";
+    cin >> n;
+    cout << "enter no o:-";
+    cin >> o;
+    cout << "enter no p:-";
+    cin >> p;
+    cout << "enter no q:-";
+    cin >> q; 
+    cout << "enter no r:-";
+    cin >> r;
+    cout << "enter no s:-";
+    cin >> s;
+    cout << "enter no t:-";
+    cin >> t;
+    cout << "enter no u:-";
+    cin >> u;
+    cout << "enter no v:-";
+    cin >> v;
+    cout << "enter no w:-";
+    cin >> w;
+    cout << "enter no x:-";
+    cin >> x;
+    cout << "enter no y:-";
+    cin >> y;
+    cout << "enter no z:-";
+    cin >> z;       
+    
+    if (a > b && a > c && a > d && a > e && a > f && a > g && a > h && a > i && a > j && a > k && a > l && a > m && a > n && a > o && a > p && a > q && a > r && a > s && a > t && a > u && a > v && a > w && a > x && a > y && a > z)
+    {
+        cout << "a is largest no" << endl;
+    }
+    else if (b > c && b > d && b > e && b > f && b > g && b > h && b > i && b > j && b > k && b > l && b > m && b > n && b > o && b > p && b > q && b > r && b > s && b > t && b > u && b > v && b > w && b > x && b > y && b > z)
+    {
+        cout << "b is largest no" << endl;
+    }
+    else if (c > d && c > e && c > f && c > g && c > h && c > i && c > j && c > k && c > l && c > m && c > n && c > o && c > p && c > q && c > r && c > s && c > t && c > u && c > v && c > w && c > x && c > y && c > z)
+    {
+        cout << "c is largest no" << endl;
+    }
+    else if (d > e && d > f && d > g && d > h && d > i && d > j && d > k && d > l && d > m && d > n && d > o && d > p && d > q && d > r && d > s && d > t && d > u && d > v && d > w && d > x && d > y && d > z)
+    {
+        cout << "d is largest no" << endl;
+    }
+    else if (e > f && e > g && e > h && e > i && e > j && e > k && e > l && e > m && e > n && e > o && e > p && e > q && e > r && e > s && e > t && e > u && e > v && e > w && e > x && e > y && e > z)
+    {
+        cout << "e is largest no" << endl;
+    }
+    else if (f > g && f > h && f > i && f > j && f > k && f > l && f > m && f > n && f > o && f > p && f > q && f > r && f > s && f > t && f > u && f > v && f > w && f > x && f > y && f > z)
+    {
+        cout << "f is largest no" << endl;
+    }
+    else if (g > h && g > i && g > j && g > k && g > l && g > m && g > n && g > o && g > p && g > q && g > r && g > s && g > t && g > u && g > v && g > w && g > x && g > y && g > z)
+    {
+        cout << "g is largest no" << endl;
+    }
+    else if (h > i && h > j && h > k && h > l && h > m && h > n && h > o && h > p && h > q && h > r && h > s && h > t && h > u && h > v && h > w && h > x && h > y && h > z)
+    {
+        cout << "h is largest no" << endl;
+    }
+    else if (i > j && i > k && i > l && i > m && i > n && i > o && i > p && i > q && i > r && i > s && i > t && i > u && i > v && i > w && i > x && i > y && i > z)
+    {
+        cout << "i is largest no" << endl;
+    }
+    else if (j > k && j > l && j > m && j > n && j > o && j > p && j > q && j > r && j > s && j > t && j > u && j > v && j > w && j > x && j > y && j > z)
+    {
+        cout << "j is largest no" << endl;
+    }
+    else if (k > l && k > m && k > n && k > o && k > p && k > q && k > r && k > s && k > t && k > u && k > v && k > w && k > x && k > y && k > z)
+    {
+        cout << "k is largest no" << endl;
+    }
+    else if (l > m && l > n && l > o && l > p && l > q && l > r && l > s && l > t && l > u && l > v && l > w && l > x && l > y && l > z)
+    {
+        cout << "l is largest no" << endl;
+    }
+    else if (m > n && m > o && m > p && m > q && m > r && m > s && m > t && m > u && m > v && m > w && m > x && m > y && m > z)
+    {
+        cout << "m is largest no" << endl;
+    }
+    else if (n > o && n > p && n > q && n > r && n > s && n > t && n > u && n > v && n > w && n > x && n > y && n > z)
+    {
+        cout << "n is largest no" << endl;
+    }
+    else if (o > p && o > q && o > r && o > s && o > t && o > u && o > v && o > w && o > x && o > y && o > z)
+    {
+        cout << "o is largest no" << endl;
+    }
+    else if (p > q && p > r && p > s && p > t && p > u && p > v && p > w && p > x && p > y && p > z)
+    {
+        cout << "p is largest no" << endl;
+    }
+    else if (q > r && q > s && q > t && q > u && q > v && q > w && q > x && q > y && q > z)
+    {
+        cout << "q is largest no" << endl;
+    }
+    else if (r > s && r > t && r > u && r > v && r > w && r > x && r > y && r > z)
+    {
+        cout << "r is largest no" << endl;
+    }
+    else if (s > t && s > u && s > v && s > w && s > x && s > y && s > z)
+    {
+        cout << "s is largest no" << endl;
+    }
+    else if (t > u && t > v && t > w && t > x && t > y && t > z)
+    {
+        cout << "t is largest no" << endl;
+    }
+    else if (u > v && u > w && u > x && u > y && u > z)
+    {
+        cout << "u is largest no" << endl;
+    }
+    else if (v > w && v > x && v > y && v > z)
+    {
+        cout << "v is largest no" << endl;
+    }
+    else if (w > x && w > y && w > z)
+    {
+        cout << "w is largest no" << endl;
+    }
+    else if (x > y && x > z)
+    {
+        cout << "x is largest no" << endl;
+    }
+    else if (y > z)
+    {
+        cout << "y is largest no" << endl;
+    }
+    else
+    {
+        cout << "z is largest no" << endl;
+    }
+    return 0;
+}
